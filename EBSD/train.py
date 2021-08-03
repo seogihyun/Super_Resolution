b50c97656a08566ab787cf1b927671cf6106b538
seogihyun <72849922+seogihyun@users.noreply.github.com>
2021-08-03 22:59:15 +0900

    Add files via upload

---
+++ EBSD/train.py
@@ -0,0 +1,182 @@
+import argparse
+import os
+import copy
+import logging
+import torch
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torchvision.utils as vutils
+from torch import nn
+from torch.cuda import amp
+from torch.utils.data.dataloader import DataLoader
+from model import EBSD
+from dataset import Dataset
+from utils import AverageMeter, ProgressMeter, calc_psnr, preprocess
+from PIL import Image
+
+
+# 에스파 테스트 이미지 경로 설정
+test_image_path = 'examples/aespa.png'
+# 에스파 테스트 이미지 불러오기
+test_image = Image.open(test_image_path).convert('RGB')
+# 에스파 테스트 이미지 전처리
+test_image = preprocess(test_image)
+
+
+
+if __name__ == '__main__':
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(format="[ %(levelname)s ] %(message)s", level=logging.INFO)
+
+    # Argparse 설정
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_file', type=str, required=True)
+    parser.add_argument('--eval_file', type=str, required=True)
+    parser.add_argument('--outputs_dir', type=str, required=True)
+    parser.add_argument('--weights_file', type=str)
+    parser.add_argument('--scale', type=int, default=4)
+    parser.add_argument('--lr', type=float, default=0.0005)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--num_epochs', type=int, default=50000)
+    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--seed', type=int, default=123)
+    parser.add_argument('--checkpoint-file', type=str, default='checkpoint-file.pth')
+    args = parser.parse_args()
+
+
+    # scale별 weight 저장 경로 설정
+    args.outputs_dir = os.path.join(args.outputs_dir, 'x{}'.format(args.scale))
+
+    # 저장 경로 없을 시 생성
+    if not os.path.exists(args.outputs_dir):
+        os.makedirs(args.outputs_dir)
+
+    cudnn.benchmark = True
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    torch.manual_seed(args.seed)
+
+    model = EBSD().to(device)
+    criterion = nn.L1Loss()
+    optimizer = optim.Adam(model.parameters(), args.psnr_lr, (0.9, 0.999))
+
+
+    # 체크포인트 weight 불러오기
+    if os.path.exists(args.checkpoint_file):
+        checkpoint = torch.load(args.checkpoint_file)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        start_epoch = checkpoint['epoch'] + 1
+        loss = checkpoint['loss']
+        best_psnr = checkpoint['best_psnr']
+
+    # 로그 인포 프린트 하기
+    logger.info(
+                f"SRDenseNet MODEL INFO:\n"
+                f"\tNumber of channels:            {args.num_channels}\n"
+                f"\tScale:                         {args.scale}\n"
+
+                f"SRDenseNet TRAINING INFO:\n"
+                f"\tTotal Epoch:                   {args.num_epochs}\n"
+                f"\tStart Epoch:                   {start_epoch}\n"
+                f"\tTrain directory path:          {args.train_file}\n"
+                f"\tTest directory path:           {args.eval_file}\n"
+                f"\tOutput weights directory path: {args.outputs_dir}\n"
+                f"\tPSNR learning rate:            {args.psnr_lr}\n"
+                f"\tPatch size:                    {args.patch_size}\n"
+                f"\tBatch size:                    {args.batch_size}\n"
+                )
+    # 스케줄러 설정 
+    psnr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    scaler = amp.GradScaler()
+
+    train_dataset = Dataset(args.train_file, args.patch_size, args.scale)
+    train_dataloader = DataLoader(dataset=train_dataset,
+                                  batch_size=args.batch_size,
+                                  shuffle=True,
+                                  num_workers=args.num_workers,
+                                  pin_memory=True)
+    eval_dataset = Dataset(args.eval_file, args.patch_size, args.scale)
+    eval_dataloader = DataLoader(
+                                dataset=eval_dataset, 
+                                batch_size=args.batch_size,
+                                shuffle=False,
+                                num_workers=args.num_workers,
+                                pin_memory=True
+                                )
+
+    best_weights = copy.deepcopy(model.state_dict())
+    best_epoch = 0
+    best_psnr = 0.0
+
+    for epoch in range(args.num_epochs):
+        model.train()
+        losses = AverageMeter(name="PSNR Loss", fmt=":.6f")
+        psnr = AverageMeter(name="PSNR", fmt=":.6f")
+        progress = ProgressMeter(
+            num_batches=len(train_dataloader),
+            meters=[losses, psnr],
+            prefix=f"Epoch: [{epoch}]"
+        )
+
+        # 트레이닝 Epoch 시작
+        for i, (lr, hr) in enumerate(train_dataloader):
+            lr = lr.to(device)
+            hr = hr.to(device)
+
+            optimizer.zero_grad()
+
+            with amp.autocast():
+                preds = model(lr)
+                loss = criterion(preds, hr)
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            losses.update(loss.item(), len(lr))
+            psnr.update(calc_psnr(preds, hr), len(lr))
+
+            if i%100==0:
+                progress.display(i)
+
+        psnr_scheduler.step()
+        
+        # epoch 별 가중치를 설정한 경로에 저장
+        torch.save(model.state_dict(), os.path.join(args.outputs_dir, 'epoch_{}.pth'.format(epoch)))
+
+        model.eval()
+        # epoch psnr reset
+        epoch_psnr = AverageMeter()
+
+        # 테스트 Epoch 시작
+        model.eval()
+        with torch.no_grad():
+            for i, (lr, hr) in enumerate(eval_dataloader):
+                lr = lr.to(device)
+                hr = hr.to(device)
+                preds = model(lr)
+                
+
+        if psnr.avg > best_psnr:
+            best_psnr = psnr.avg
+            torch.save(
+                model.state_dict(), os.path.join(args.outputs_dir, 'best.pth')
+            )
+
+        torch.save(
+            {
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss,
+                'best_psnr': best_psnr,
+            }, os.path.join(args.outputs_dir, 'epoch_{}.pth'.format(epoch))
+        )
+
+        """ 에스파 이미지 테스트 """
+        with torch.no_grad():
+            lr = test_image.to(device)
+            preds = model(lr)
+            vutils.save_image(preds.detach(), os.path.join(args.outputs_dir, f"PSNR_{epoch}.png"))
---
+++ EBSD/utils.py
@@ -0,0 +1,116 @@
+import torch
+import numpy as np
+
+
+# 이미지 파일 확장자 검수 함수
+def check_image_file(filename: str):
+    return any(filename.endswith(extension) for extension in [".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff", ".JPG", ".JPEG", ".PNG", ".BMP"])
+
+# 전처리 과정 함수
+def preprocess(img):
+    # uInt8 -> float32로 변환
+    x = np.array(img).astype(np.float32)
+    x = x.transpose([2,0,1])
+    # Normalize x 값
+    x /= 255.
+    # 넘파이 x를 텐서로 변환
+    x = torch.from_numpy(x)
+    # x의 차원의 수 증가
+    x = x.unsqueeze(0)
+    # x 값 반환
+    return x
+
+def calc_patch_size(func):
+    def wrapper(args):
+        if args.scale == 2:
+            args.patch_size = 10
+        elif args.scale == 3:
+            args.patch_size = 7
+        elif args.scale == 4:
+            args.patch_size = 6
+        else:
+            raise Exception('Scale Error', args.scale)
+        return func(args)
+    return wrapper
+
+def convert_rgb_to_y(img, dim_order='hwc'):
+    if dim_order == 'hwc':
+        return 16. + (64.738 * img[..., 0] + 129.057 * img[..., 1] + 25.064 * img[..., 2]) / 256.
+    else:
+        return 16. + (64.738 * img[0] + 129.057 * img[1] + 25.064 * img[2]) / 256.
+
+def convert_rgb_to_ycbcr(img, dim_order='hwc'):
+    if dim_order == 'hwc':
+        y = 16. + (64.738 * img[..., 0] + 129.057 * img[..., 1] + 25.064 * img[..., 2]) / 256.
+        cb = 128. + (-37.945 * img[..., 0] - 74.494 * img[..., 1] + 112.439 * img[..., 2]) / 256.
+        cr = 128. + (112.439 * img[..., 0] - 94.154 * img[..., 1] - 18.285 * img[..., 2]) / 256.
+    else:
+        y = 16. + (64.738 * img[0] + 129.057 * img[1] + 25.064 * img[2]) / 256.
+        cb = 128. + (-37.945 * img[0] - 74.494 * img[1] + 112.439 * img[2]) / 256.
+        cr = 128. + (112.439 * img[0] - 94.154 * img[1] - 18.285 * img[2]) / 256.
+    return np.array([y, cb, cr]).transpose([1, 2, 0])
+
+def convert_ycbcr_to_rgb(img, dim_order='hwc'):
+    if dim_order == 'hwc':
+        r = 298.082 * img[..., 0] / 256. + 408.583 * img[..., 2] / 256. - 222.921
+        g = 298.082 * img[..., 0] / 256. - 100.291 * img[..., 1] / 256. - 208.120 * img[..., 2] / 256. + 135.576
+        b = 298.082 * img[..., 0] / 256. + 516.412 * img[..., 1] / 256. - 276.836
+    else:
+        r = 298.082 * img[0] / 256. + 408.583 * img[2] / 256. - 222.921
+        g = 298.082 * img[0] / 256. - 100.291 * img[1] / 256. - 208.120 * img[2] / 256. + 135.576
+        b = 298.082 * img[0] / 256. + 516.412 * img[1] / 256. - 276.836
+    return np.array([r, g, b]).transpose([1, 2, 0])
+
+def preprocess(img, device):
+    img = np.array(img).astype(np.float32)
+    ycbcr = convert_rgb_to_ycbcr(img)
+    x = ycbcr[..., 0]
+    x /= 255.
+    x = torch.from_numpy(x).to(device)
+    x = x.unsqueeze(0).unsqueeze(0)
+    return x, ycbcr
+
+def calc_psnr(img1, img2):
+    return 10. * torch.log10(1. / torch.mean((img1 - img2) ** 2))
+
+# Copy from https://github.com/pytorch/examples/blob/master/imagenet/main.py
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+# Copy from https://github.com/pytorch/examples/blob/master/imagenet/main.py
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
