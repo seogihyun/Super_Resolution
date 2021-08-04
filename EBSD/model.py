76f4781c770b94aa179fd4f27c392419b17efc79
seogihyun <72849922+seogihyun@users.noreply.github.com>
2021-08-04 11:44:01 +0900

    Update model.py

--- EBSD/model.py
+++ EBSD/model.py
@@ -6,14 +6,17 @@ class _Residual_Block(nn.Module):
     def __init__(self):
         super(_Residual_Block, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=2, bias=False)
+        self.conv1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=2, bias=False)
-  
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+
     def forward(self, x):
         identity_data = x
+
         output = self.relu(self.conv1(x))
+
         output = self.conv2(output)
+
         output = torch.add(output, identity_data)
         return output 
 
@@ -22,23 +25,23 @@ class EBSD(nn.Module):
     def __init__(self):
         super(EBSD, self).__init__()
 
-        self.conv_input = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, stride=1, padding=8, bias=False)
+        self.conv_input = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, stride=1, padding=9//2, bias=False)
         self.relu = nn.ReLU(inplace=True)
       
         self.residual = self.make_layer(_Residual_Block, 16)
 
-        self.conv_mid = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=2, bias=False)
+        self.conv_mid = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
 
         self.upscale4x = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=2, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
             nn.PixelShuffle(2),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=2, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
             nn.PixelShuffle(2),
             nn.ReLU(inplace=True),
         )
 
-        self.conv_output = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=9, stride=1, padding=8, bias=False)
+        self.conv_output = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=9, stride=1, padding=9//2, bias=False)
         
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -58,7 +61,7 @@ class EBSD(nn.Module):
         residual = out
         out = self.residual(out)
         out = self.conv_mid(out)
-        out = torch.add(out,residual)
+        out = torch.add(out, residual)
         out = self.upscale4x(out)
         out = self.conv_output(out)
         return out
