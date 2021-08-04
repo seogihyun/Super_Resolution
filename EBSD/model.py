da21a804e0cebb1a91c9007b9d9b120ce03b2592
seogihyun <72849922+seogihyun@users.noreply.github.com>
2021-08-04 23:32:00 +0900

    Update model.py

--- EBSD/model.py
+++ EBSD/model.py
@@ -3,20 +3,26 @@ import torch.nn as nn
 import math
 
 class _Residual_Block(nn.Module):
-    def __init__(self):
+    def __init__(self, bn=True):
         super(_Residual_Block, self).__init__()
 
+        self.bn = bn
         self.conv1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        if bn:
+            self.bn1 = nn.BatchNorm2d(64)
+            self.bn2 = nn.BatchNorm2d(64)
 
     def forward(self, x):
         identity_data = x
-
-        output = self.relu(self.conv1(x))
-
+        output = self.conv1(x)
+        if self.bn:
+            output =self.bn1(output)
+        output = self.relu(output)
         output = self.conv2(output)
-
+        if self.bn:
+            output = self.bn2(output)
         output = torch.add(output, identity_data)
         return output 
 
@@ -26,35 +32,40 @@ class EBSD(nn.Module):
         super(EBSD, self).__init__()
 
         self.conv_input = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, stride=1, padding=9//2, bias=False)
-        self.relu = nn.ReLU(inplace=True)
-      
+        self.relu = nn.LeakyReLU(0.2, inplace=True)
         self.residual = self.make_layer(_Residual_Block, 16)
-
         self.conv_mid = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
-
         self.upscale4x = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
             nn.PixelShuffle(2),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
             nn.PixelShuffle(2),
-            nn.ReLU(inplace=True),
+            nn.LeakyReLU(0.2, inplace=True),
         )
-
         self.conv_output = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=9, stride=1, padding=9//2, bias=False)
-        
+
+        # init the weight of conv2d
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
+                #init.orthogonal(m.weight, math.sqrt(2))
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
                 if m.bias is not None:
                     m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                if m.bias is not None:
+                    m.bias.data.zero_()
 
-    def make_layer(self, block, num_of_layer):
+
+    def make_layer(self, block, bn, num_of_layer):
         layers = []
         for _ in range(num_of_layer):
-            layers.append(block())
+            layers.append(block(bn = bn))
         return nn.Sequential(*layers)
+
+
 
     def forward(self, x):
         out = self.relu(self.conv_input(x))
@@ -64,4 +75,71 @@ class EBSD(nn.Module):
         out = torch.add(out, residual)
         out = self.upscale4x(out)
         out = self.conv_output(out)
+        return out
+
+
+class SRResNet_RGBY(nn.Module):
+    def __init__(self, in_channels=4, out1_channels=3, out2_channels=1, bn=True):
+        """
+        in and out channel = io_channels;RGB mode io_channels =3 ,Y channel io_channels = 1
+        """
+        super(SRResNet_RGBY, self).__init__()
+        
+        self.bn = bn
+        self.conv_input = nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=9, stride=1, padding=9//2, bias=False)
+        #self.relu = nn.LeakyReLU(0.2, inplace=True)
+        self.relu = nn.PReLU(num_parameters=1, init=0.2)
+        
+        self.residual = self.make_layer(_Residual_Block, bn, 16)
+
+        self.conv_mid = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        if self.bn:
+            self.bn_mid = nn.BatchNorm2d(64)
+
+        self.upscale4x = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.PixelShuffle(2),
+            #nn.LeakyReLU(0.2, inplace=True),
+            nn.PReLU(num_parameters=1, init=0.2),
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.PixelShuffle(2),
+            #nn.LeakyReLU(0.2, inplace=True),
+            nn.PReLU(num_parameters=1, init=0.2),
+        )
+
+        self.conv_output = nn.Conv2d(in_channels=64, out_channels=out1_channels, kernel_size=9, stride=1, padding=4, bias=False)
+        self.conv_output2 = nn.Conv2d(in_channels=out1_channels, out_channels=out2_channels, kernel_size=1, stride=1, padding=0, bias=False)
+        
+        # init the weight of conv2d
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                #init.orthogonal(m.weight, math.sqrt(2))
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+                
+    def make_layer(self, block, bn, num_of_layer):
+        layers = []
+        for _ in range(num_of_layer):
+            layers.append(block(bn = bn))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out1 = self.relu(self.conv_input(x))
+        #residual = out.clone()
+        out = self.residual(out1)
+        out = self.conv_mid(out)
+        if self.bn:
+            out = self.bn_mid(out)
+        out = torch.add(out,out1)
+        out = self.upscale4x(out)
+        rgb = self.conv_output(out)
+        y = self.conv_output2(rgb)
+        print(y.size())
+        out = torch.cat((rgb,y), dim = 1)   # channel
         return out
