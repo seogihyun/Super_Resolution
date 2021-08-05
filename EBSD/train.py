aa42c88d3f38ea29a25558127e593b341c519c59
seogihyun <72849922+seogihyun@users.noreply.github.com>
2021-08-05 09:44:30 +0900

    Update train.py

--- EBSD/train.py
+++ EBSD/train.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     parser.add_argument('--scale', type=int, default=4)
     parser.add_argument('--lr', type=float, default=0.0005)
     parser.add_argument('--patch_size', type=int, default=64)
-    parser.add_argument('--batch_size', type=int, default=40)
+    parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--num_epochs', type=int, default=50000)
     parser.add_argument('--num_channels', type=int, default=3)
     parser.add_argument('--num_workers', type=int, default=8)
